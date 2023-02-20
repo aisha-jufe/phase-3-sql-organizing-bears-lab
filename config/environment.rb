8b0384d961ebdb0e26dbeee988c87c6cbefe0fbf
aisha-jufe <aishajucfe287>
2023-02-20 16:02:03 +0300

    complted

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,4 @@ require 'base64'
 Bundler.require
 
 require_relative '../lib/sql_queries'
+
---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) 
+VALUES (1, 'Mr. Chocolate', 5, 'M', 'brown', 'grumpy', true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) 
+VALUES (2, 'Rowdy', 3, 'M', 'black', 'playful', true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) 
+VALUES (3, 'Tabitha', 7, 'F', 'white', 'shy', true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) 
+VALUES (4, 'Sergeant Brown', 9, 'M', 'brown', 'strict', true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) 
+VALUES (5, 'Melissa', 2, 'F', 'blonde', 'energetic', true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) 
+VALUES (6, 'Grinch', 4, 'M', 'green', 'grumpy', true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) 
+VALUES (7, 'Wendy', 6, 'F', 'brown', 'playful', true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) 
+VALUES (8, 'unnamed', NULL, 'F', 'brown', 'unknown', false);
+
+INSERT INTO bears (name, age, sex) VALUES (NULL, NULL, NULL);
